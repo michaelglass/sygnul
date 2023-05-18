9ac5e593e7056ed62e30023da5648fbdc5a82932
Michael Glass <me@mike.is>
2023-05-18 14:52:49 -0500

    stop using socket

--- post_message.rb
+++ post_message.rb
@@ -12,34 +12,38 @@ params = {
 }
 
 # Set the URI and path
-uri = URI("http://localhost:80/")
+if true
+  uri = URI("http://localhost:80/")
 
-# Create the HTTP objects
-http = Net::HTTP.new(uri.host, uri.port)
+  # Create the HTTP objects
+  http = Net::HTTP.new(uri.host, uri.port)
 
-# Create the request object and set the body as the JSON payload
-request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-request.body = params.to_json
+  # Create the request object and set the body as the JSON payload
+  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  request.body = params.to_json
 
-http.request(request)
+  http.request(request)
+else
 
-# # Set the request target
-# host = 'localhost'
-# port = 80
-# path = "/"
+# Set the request target
+host = 'localhost'
+port = 80
+path = "/"
 
-# # Create the POST request
-# request = "POST #{path} HTTP/1.1\r\n" +
-#           "Host: #{host}\r\n" +
-#           "Content-Type: application/json\r\n" +
-#           "Content-Length: #{params.size}\r\n\r\n" +
-#           "#{params.to_json}\r\n"
+# Create the POST request
+request = "POST #{path} HTTP/1.1\r\n" +
+          "Host: #{host}\r\n" +
+          "Content-Type: application/json\r\n" +
+          "Content-Length: #{params.size}\r\n\r\n" +
+          "#{params.to_json}\r\n"
 
 # Create a TCP socket
-# socket = TCPSocket.new(host, port)
+socket = TCPSocket.new(host, port)
 
-# # Send the request
-# socket.print(request)
+# Send the request
+puts socket.read
+socket.print(request)
 
-# # No longer waiting for or printing the response
-# socket.close
+# No longer waiting for or printing the response
+socket.close
+end
